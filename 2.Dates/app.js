ea5beddf1885df531d4b88a9ea29470472b957da
kokenkubra <kokenkubraho@gmail.com>
2022-10-27 16:57:44 +0200

    second

--- 2.Dates/app.js
+++ 2.Dates/app.js
@@ -28,16 +28,84 @@ console.log(days(date_1, date_2) +" days since your are alive");
 // const time = updatedHours.toLocaleTimeString();
 // console.log(dates);
 // console.log(time);
-const inputhours = document.querySelector('#hourss');
-const futurTime = () => {
-    let hours = new Date().getTime();
+// const inputhours = document.querySelector('#hourss');
+// const futurTime = () => {
+//     let hours = new Date().getTime();
     
-    let updatedHours = new Date(hours + (inputhours.value) *1000 * 3600);
-    const dates = updatedHours.toDateString();
-    const time = updatedHours.toLocaleTimeString();
-    console.log(dates);
-    console.log(time);
-}
-hourss.addEventListener('keyup', futurTime);
+//     let updatedHours = new Date(hours + (inputhours.value) *1000 * 3600);
+//     const dates = updatedHours.toDateString();
+//     const time = updatedHours.toLocaleTimeString();
+//     console.log(dates);
+//     console.log(time);
+// }
+// hourss.addEventListener('keyup', futurTime);
 
 // -- exe4 --//
+const refresh = () => {
+const todayday = document.querySelector(".DAY");
+const todaydte = document.querySelector(".DATE");
+const todayyear = document.querySelector(".YEAR");
+
+const todayTime = document.querySelector(".time");
+let newDte = new Date();
+
+const weekdays =["Sunday",
+         "Monday",
+         "Tuesday",
+         "Wednesday",
+        "Thursday",
+        "Friday",
+         "Saturday"]
+
+
+    const r = weekdays[newDte.getDay()];
+    todayday.innerHTML = r;
+    const D = newDte.getUTCDate();
+    const M = newDte.getUTCMonth();
+    todaydte.innerHTML = D + "/" + M;
+    todayyear.innerHTML = newDte.getFullYear();
+    todayTime.innerHTML = newDte.toLocaleTimeString();
+    
+}
+
+const is24Hour = false,
+    timeDisplay = document.querySelector(".time");
+   
+
+
+const changeTo12 = () => {
+        let currentDate = new Date(),
+        hours = currentDate.getHours(),
+        minutes = currentDate.getMinutes(),
+        seconds = currentDate.getSeconds();
+
+    if(is24Hour) {
+        timeDisplay.innerText = hours + ":" + minutes + ":" + seconds;
+    }
+    else {
+        var suffix = hours >= 12 ? "PM" : "AM",
+            hours12 = hours % 12;
+        
+        timeDisplay.innerText = hours12 + ":" + minutes + ":" + seconds + " " + suffix;
+    }
+
+}
+
+var changeFormat = function() {
+    is24Hour = !is24Hour;
+}
+
+setInterval(refresh, 1000)
+ 
+const myStoptime24 = () => {
+   clearInterval(refresh);
+}
+
+setInterval(changeTo12, 1000)
+
+const myStoptime12 = () => {
+    clearInterval(changeTo12);
+}
+ timeDisplay.addEventListener('click', changeTo12);
+window.setInterval(changeTo12, 100);
+
